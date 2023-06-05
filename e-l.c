928c50a70e0ef4658f860866155ad0f04bc58899
Yasser-Jemli <92098387+Yasser-Jemli@users.noreply.github.com>
2023-06-05 21:00:52 +0200

    Add files via upload

---
+++ e-l.c
@@ -0,0 +1,59 @@
+
+
+
+// By : Yasser JEMLI //--05-05-2023 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+
+
+
+int main(int argc, char* argv[])
+{
+	if(argc != 2){printf("error\n"); exit(EXIT_FAILURE);}
+
+	printf(" ************* Communication via les pipes anonymes ************* \n");
+	printf(" Proc courant : PID = %d et PPID = %d ----- \n", getpid(),getppid());
+	printf(" Proc courant : je vais créer un pipe anonyme via l'appel système pipe() \n");
+	int fd[2];
+	int ret = pipe(fd);// Appel système de création de tube anonymes
+	if(ret == -1) {perror("TUBE"); exit(EXIT_FAILURE);}
+	//2
+	pid_t val=fork();
+	if(val==-1){perror("FORK"); exit(EXIT_FAILURE);}
+	//3
+	if(val == 0)
+	{//FILS
+	close(fd[0]);
+	if(write(fd[1],argv[1], strlen(argv[1]))==-1)
+		{
+			perror("wr");
+			exit(EXIT_FAILURE);
+		}
+
+	printf("\t\t FILS : je viens d'envoyer un message à mon père et je termine dans 5 sec\n");
+	sleep(5);
+	}else{
+	//PERE :
+	printf("PERE : j'attends la terminaison du proc FILS \n");
+	wait(NULL);
+	close(fd[1]);
+	char BUFFER[10];
+	int nbOctectLu;
+	bzero(BUFFER,10);
+	nbOctectLu=read(fd[0],BUFFER, 10);
+	while(nbOctect)
+	if(nbOctectLu==-1)
+		{
+			perror("Rd");
+			exit(EXIT_FAILURE);
+		}
+	BUFFER[nbOctectLu]='\0';
+	printf("Pere : je viens de recevoir le message --> %s \n", BUFFER);
+	}
+return 0;
+}
+
